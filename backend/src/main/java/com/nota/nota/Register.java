dd9f041d65459f7f7cb2515fe00043e5c12d9688
= <brochettes40@outlook.fr>
2025-07-12 16:17:18 +0200

    Backend - restoer edited

--- backend/src/main/java/com/nota/nota/Register.java
+++ backend/src/main/java/com/nota/nota/Register.java
@@ -1,12 +1,13 @@
 package com.nota.nota;
 
-import dao.ArticleRepository;
-import entities.Article;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import dao.ArticleRepository;
+import entities.Article;
 
 @RestController
 class Register {
